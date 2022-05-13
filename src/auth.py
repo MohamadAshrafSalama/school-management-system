561c68c6bb8e4d27c473914e1906d815461a99db
Mohamad Ashraf Salama <mohamadashrafaast@gmail.com>
2022-05-13 10:19:36 +0000

    Add input validation and password change to auth

--- src/auth.py
+++ src/auth.py
@@ -6,7 +6,21 @@ def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 
+def validate_username(username):
+    if not username or len(username) < 3:
+        raise ValueError("Username must be at least 3 characters.")
+    if not username.isalnum():
+        raise ValueError("Username must be alphanumeric.")
+    return True
+
+
 def create_user(username, password, role):
+    validate_username(username)
+    if len(password) < 4:
+        raise ValueError("Password must be at least 4 characters.")
+    if role not in ('admin', 'teacher', 'student'):
+        raise ValueError("Invalid role.")
+
     conn = get_connection()
     try:
         cursor = conn.execute(
@@ -39,6 +53,26 @@ def get_user_by_id(user_id):
     row = conn.execute("SELECT * FROM users WHERE id = ?", (user_id,)).fetchone()
     conn.close()
     return dict(row) if row else None
+
+
+def change_password(user_id, old_password, new_password):
+    conn = get_connection()
+    user = conn.execute(
+        "SELECT * FROM users WHERE id = ? AND password = ?",
+        (user_id, hash_password(old_password))
+    ).fetchone()
+    if not user:
+        conn.close()
+        raise ValueError("Current password is incorrect.")
+    if len(new_password) < 4:
+        conn.close()
+        raise ValueError("New password must be at least 4 characters.")
+    conn.execute(
+        "UPDATE users SET password = ? WHERE id = ?",
+        (hash_password(new_password), user_id)
+    )
+    conn.commit()
+    conn.close()
 
 
 def create_default_admin():
